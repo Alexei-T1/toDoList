ae1243d523a352a7051c1aa05ca7b9609a3c29a6
Alexei-T1 <turencko.alexei@yandex.ru>
2024-05-10 18:39:51 +0400

    00 add styles default base structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+console.log("Hello!");
+
+import "./styles/main.css";
+import { load } from "./scripts/load.js";
+
+
+const elBody = document.body;
+const elLoad = load();
+
+window.addEventListener('load', () => {
+  elLoad.replaceWith(tempLoaded);
+});
+
+
+elBody.append(elLoad);
+
+
+const tempLoaded = (()=>{
+
+return document.createElement('div').innerText = "loaded";
+
+})();
+
+
+
---
+++ src/scripts/load.js
@@ -0,0 +1,8 @@
+export const clLoad = "clLoad";
+
+export const load = (load = 'load... ') => {
+  const elLoad = document.createElement('div');
+  elLoad.innerText = load;
+  elLoad.classList = "clLoad";
+  return elLoad;
+}
