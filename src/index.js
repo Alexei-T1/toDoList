880452f7743a803abc05daa3b94350b73a6d69a0
Alexei-T1 <turencko.alexei@yandex.ru>
2024-05-19 19:48:20 +0400

    start created load start  window

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ console.log("Hello!");
 import "./styles/main.css";
 import { load } from "./scripts/load.js";
 import { board } from "./scripts/boardBack.js";
-
+import { loadWindow } from "./scripts/startWindows.js";
 
 const elBody = document.body;
 const elLoad = load();
 const elBoard = board();
+const elLoadWindow = loadWindow();
 
 window.addEventListener('load', () => {
-  elLoad.replaceWith(tempLoaded);
+  elLoad.replaceWith(startLoaded);
 });
 
 
 elBody.append(elLoad);
 
 
-const tempLoaded = (()=>{
- const loaded =  document.createElement('div');
- loaded.innerText = "loaded";
-return loaded;
+const startLoaded = (()=>{
+ const elLoaded =  document.createElement('div');
+return elLoaded;
 
 })();
 
-console.log(tempLoaded);
-tempLoaded.append(elBoard);
+console.log(startLoaded);
+startLoaded.append(elBoard, elLoadWindow);
 
